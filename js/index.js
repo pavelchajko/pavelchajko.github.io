8394273065fdd9ad30a40e88ad99145c1ecb144a
pavelchajko <pavel.gichevski@gmail.com>
2016-10-10 18:26:37 +0200

    Northern belting company website

---
+++ js/index.js
@@ -0,0 +1,124 @@
+$(document).ready(function(){
+    $(".navbar-toggle").click(function(){
+        $("#jq").toggle();
+      });
+      $("#profileName").mouseenter(function(){
+        $(this).css("background-color", "#ffffff");
+   });
+  
+   $("#profileName").mouseleave(function(){
+        $(this).css("background-color", "#C0C0C0");
+   });
+  
+  $("#a1").mouseenter(function(){
+     $("#a1").css("background-color", "#FFFFFF");
+   }); 
+  $("#a1").mouseleave(function(){
+        $("#a1").css("background-color", "#e7e7e7");   
+   });
+  
+  
+  $("#a2").mouseenter(function(){
+     $("#a2").css("background-color", "#FFFFFF");
+   });
+  $("#a2").mouseleave(function(){
+        $("#a2").css("background-color", "#e7e7e7");   
+   });
+  
+  $("#a3").mouseenter(function(){
+     $("#a3").css("background-color", "#FFFFFF");
+   }); 
+  $("#a3").mouseleave(function(){
+        $("#a3").css("background-color", "#e7e7e7");   
+   });
+  
+  $("#a4").mouseenter(function(){
+     $("#a4").css("background-color", "#FFFFFF");
+   }); 
+  $("#a4").mouseleave(function(){
+        $("#a4").css("background-color", "#e7e7e7");   
+   });
+  
+ 
+  
+  $("#social1").mouseenter(function(){
+     $("#social1").css({"background-color": "#000 ", "transform":"scale(1.1)","color":"#FFFFFF"});
+   }); 
+   $("#social1").mouseleave(function(){
+     $("#social1").css({"background-color": "#FFFFFF","transform":"scale(1)","color":"#000"});
+   });
+  
+  
+   $("#social2").mouseenter(function(){
+     $("#social2").css({"background-color": "#000 ", "transform":"scale(1.1)","color":"#FFFFFF"});
+   });
+    $("#social2").mouseleave(function(){
+     $("#social2").css({"background-color": "#FFFFFF","transform":"scale(1)","color":"#000"});
+   });
+  
+  
+   $("#social3").mouseenter(function(){
+     $("#social3").css({"background-color": "#000 ", "transform":"scale(1.1)","color":"#FFFFFF"});
+   });
+    $("#social3").mouseleave(function(){
+     $("#social3").css({"background-color": "#FFFFFF","transform":"scale(1)","color":"#000"});
+   });
+  
+   $("#social4").mouseenter(function(){
+     $("#social4").css({"background-color": "#000 ", "transform":"scale(1.1)","color":"#FFFFFF"});
+   });
+    $("#social4").mouseleave(function(){
+     $("#social4").css({"background-color": "#FFFFFF","transform":"scale(1)","color":"#000"});
+   });
+  
+  
+  $("#social5").mouseenter(function(){
+     $("#social5").css({"background-color": "#000 ", "transform":"scale(1.1)","color":"#FFFFFF"});
+   }); 
+  $("#social5").mouseleave(function(){
+     $("#social5").css({"background-color": "#FFFFFF","transform":"scale(1)","color":"#000"});
+   });
+  
+  
+    $("#social6").mouseenter(function(){
+     $("#social6").css({"background-color": "#000 ", "transform":"scale(1.1)","color":"#FFFFFF"});
+   }); 
+  $("#social6").mouseleave(function(){
+     $("#social6").css({"background-color": "#FFFFFF","transform":"scale(1)","color":"#000"});
+   });
+  
+  
+    $("#social7").mouseenter(function(){
+     $("#social7").css({"background-color": "#000 ", "transform":"scale(1.1)","color":"#FFFFFF"});
+   }); 
+  $("#social7").mouseleave(function(){
+     $("#social7").css({"background-color": "#FFFFFF","transform":"scale(1)","color":"#000"});
+   });
+  
+    $("#social8").mouseenter(function(){
+     $("#social8").css({"background-color": "#000 ", "transform":"scale(1.1)","color":"#FFFFFF"});
+   });  
+  $("#social8").mouseleave(function(){
+     $("#social8").css({"background-color": "#FFFFFF","transform":"scale(1)","color":"#000"});
+   });
+  
+  
+  $(".list1").mouseenter(function(){
+     $(this).css({"background-color": "#000 ", "transform":"scale(1.1)","color":"#FFFFFF"});
+   });  
+  $(".list1").mouseleave(function(){
+     $(this).css({"background-color": "#000 ", "transform":"scale(1)","color":"#FFFFFF"});
+   });  
+  
+  
+   $(".list2").mouseenter(function(){
+     $(this).css({"background-color": "#000 ", "transform":"scale(1.1)","color":"#FFFFFF",});
+   });  
+  $(".list2").mouseleave(function(){
+     $(this).css({"background-color": "#000 ", "transform":"scale(1)","color":"#FFFFFF"});
+   });  
+  
+  
+  
+  
+ });
